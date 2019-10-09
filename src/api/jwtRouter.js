c13abc093158ca6304b37d2517640fde4d708951
Samuel Šramko <samuel.sramko@tatum.io>
2019-10-09 15:43:11 +0200

    NOISSUE - JWT token response format

--- src/api/jwtRouter.js
+++ src/api/jwtRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/:key/:secret', ({ params }, res) => {
   const { key, secret } = params;
   const token = jwt.sign({ apiKey: key, created: Date.now() }, secret);
-  res.json(token);
+  res.json({token});
 });
 
 module.exports = router;
