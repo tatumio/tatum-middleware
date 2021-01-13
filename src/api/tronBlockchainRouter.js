56e83c1e6c7b67478aad52eda05cb358e284844e
Samuel Sramko <samuel.sramko@tatum.io>
2021-01-13 15:13:27 +0100

    TRON TRC10-20 support

--- src/api/tronBlockchainRouter.js
+++ src/api/tronBlockchainRouter.js
@@ -21,4 +21,74 @@ router.post('/transaction', async ({body, headers}, res) => {
   }
 });
 
+router.post('/trc10/transaction', async ({body, headers}, res) => {
+  try {
+    process.env.TATUM_API_KEY = headers['x-api-key'];
+    res.status(200).json(await tatum.sendTronTrc10Transaction(process.env.MODE !== 'MAINNET', body));
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({
+      message: `Unable to prepare transaction. Possible error: ${e.message || e}`,
+      statusCode: 400,
+      errorCode: 'tron.error',
+    });
+  }
+});
+
+router.post('/trc20/transaction', async ({body, headers}, res) => {
+  try {
+    process.env.TATUM_API_KEY = headers['x-api-key'];
+    res.status(200).json(await tatum.sendTronTrc20Transaction(process.env.MODE !== 'MAINNET', body));
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({
+      message: `Unable to prepare transaction. Possible error: ${e.message || e}`,
+      statusCode: 400,
+      errorCode: 'tron.error',
+    });
+  }
+});
+
+router.post('/trc10/deploy', async ({body, headers}, res) => {
+  try {
+    process.env.TATUM_API_KEY = headers['x-api-key'];
+    res.status(200).json(await tatum.createTronTrc10Transaction(process.env.MODE !== 'MAINNET', body));
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({
+      message: `Unable to prepare transaction. Possible error: ${e.message || e}`,
+      statusCode: 400,
+      errorCode: 'tron.error',
+    });
+  }
+});
+
+router.post('/trc20/deploy', async ({body, headers}, res) => {
+  try {
+    process.env.TATUM_API_KEY = headers['x-api-key'];
+    res.status(200).json(await tatum.createTronTrc20Transaction(process.env.MODE !== 'MAINNET', body));
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({
+      message: `Unable to prepare transaction. Possible error: ${e.message || e}`,
+      statusCode: 400,
+      errorCode: 'tron.error',
+    });
+  }
+});
+
+router.post('/freezeBalance', async ({body, headers}, res) => {
+  try {
+    process.env.TATUM_API_KEY = headers['x-api-key'];
+    res.status(200).json(await tatum.freezeTronTransaction(process.env.MODE !== 'MAINNET', body));
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({
+      message: `Unable to prepare transaction. Possible error: ${e.message || e}`,
+      statusCode: 400,
+      errorCode: 'tron.error',
+    });
+  }
+});
+
 module.exports = router;
